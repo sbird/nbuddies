1c8b283d816ea48b06151a0918423a3446e2964d
UnwieldyLizard <unwieldylizard@gmail.com>
2025-10-06 10:34:08 -0700

    first pass at forces

---
+++ black_holes.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Black_hole:
+    def __init__(self, mass: float, position: list[float], velocity: list[float]):
+        """
+        Initialized Black Hole, sets mass and intital position, velocity, and acceleration.
+
+        Parameters
+        ----------
+        mass : float
+            mass of black hole
+        position : list[float]
+            position vector of black hole
+        velocity : list[float])
+            velocity vector of black hole
+        """
+        self.mass = mass
+        self.position = np.array(position)
+        self.velocity = np.array(velocity)
+        self.acceleration = np.zeros(3)
+
+    def __eq__(self, other):
+        """
+        Checks if two blacks holes are the same by checking all except acceleration.
+
+        Parameters
+        ----------
+        other : Black_hole
+            The black hole being compared to
+
+        Returns
+        -------
+        bool
+            Whether or not these black holes are the same.
+        """
+        if other.mass != self.mass:
+            return False
+        
+        for i in range(3):
+            if other.position[i] != self.position[i]:
+                return False
+            
+            if other.velocity[i] != self.velocity[i]:
+                return False
+            
+        return True
+
---
+++ forces.py
@@ -0,0 +1,50 @@
+from black_holes import *
+
+def recalculate_acceleration_due_to_gravity(data: list[Black_hole]):
+    """
+    Recalculates accelerations of all black holes
+
+    Parameters
+    ----------
+    data : list[Black_hole]
+        list of black holes to be target and basis of acceleration recalculation
+    """
+    for BH_i in data:
+        BH_i.acceleration = np.zeros(3)
+        for BH_j in data:
+            if BH_i == BH_j:
+                print("pass")
+                continue
+            print(BH_i.mass, BH_j.mass)
+            BH_i.acceleration += calculate_acceleration_from_one_body(BH_i, BH_j)
+
+def calculate_acceleration_from_one_body(target: Black_hole, source: Black_hole):
+    """
+    Calculate the accelation of one black hole due to another
+
+    Parameters
+    ----------
+    target : Black_hole
+        The black hole whose acceleration is being calculated
+    source : Black_hole
+        The black hole whose gravity is the source of the acceleration
+    """
+    GG = 4.301e-6 # Newton constant km^2 kpc / Msun s^2
+    displacement = source.position - target.position
+    return GG * source.mass * displacement / (vector_magnitude(displacement) ** 3)
+
+def vector_magnitude(vec: list[float]):
+    """
+    Computes magnitude of a vector.
+
+    Parameters
+    ----------
+    vec : list[float]
+        The vectors to have it's magnitude calculated
+    
+    Returns
+    -------
+    float
+        The magnitude of the vector
+    """
+    return np.sqrt(np.sum(vec*vec))
