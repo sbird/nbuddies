e1c986ce2c309c0b8c0873ec575a8cc17e7bc975
GoureeSankar <gouree.sath@gmail.com>
2025-10-31 17:30:49 -0700

    changes to test_run.py

--- test_run.py
+++ test_run.py
@@ -10,14 +10,96 @@ import numpy as np
 from evolution2 import simulation
 from ICs import generate_plummer_initial_conditions
 from visualizations import movie_3D
-from pint import UnitRegistry 
-import pickle
 import shutil
 import os
 import pickle as pkl
+import time
+from visualizations_comparisons import movie_3D_comparison
+from BlackHoles_Struct import BlackHole
+# from binary import generate_binary_ICs
 
-ureg = UnitRegistry()
+start = time.time()
 
+def test_simulation_run(n_bh = 3, do_tree = True, do_brute = False, 
+                        do_comparison = False, do_binary = False):
+    
+    tot = 5e16   #total time in seconds for integration
+    nstep = 10  #number of steps per batch
+    eta = 0.1   #adaptive time step parameter
+
+    output_folder_data_tree = "./data_tree/"
+    output_folder_data_brute = "./data_brute/"
+    output_folder_movie = "./movie_dump/"
+
+    end = []
+
+    #check if the folder exists, if yes delete and make a new one, else just make a new one 
+    def check_directory(folder_path):
+        if os.path.exists(folder_path):
+            shutil.rmtree(folder_path)  # Remove the folder and all its contents
+
+        os.makedirs(folder_path)  #make new empty folder
+
+    path_to_save_pkl_file = "./"
+
+    if do_binary:
+        custom_vals = { 'N': 2,
+                'mass': np.array([1.0e7, 1.0e7]), 
+                'position': np.array([[1., 0., 0.], [-1., 0., 0.]]), 
+                'velocity': np.array([[0. ,3.2791 ,0.], [0. ,-3.2791 ,0.]])}
+        # generate_binary_ICs(N_BH = 2, custom_vals = custom_vals, analy_sets = False)  #some problem with binary.py
+        init_BH_masses = custom_vals['mass']    
+        init_BH_positions = custom_vals['position']
+        init_BH_velocities = custom_vals['velocity']
+         ## Load the custom_vals into Class objects
+        list_of_BH = []
+        n = custom_vals['N']
+        for i in range(n):
+            BH = BlackHole( init_BH_masses[i], init_BH_positions[i], init_BH_velocities[i] )
+            list_of_BH.append(BH)
+
+        data = dict()
+        data['data'] = list_of_BH
+        ## Save the file
+        with open('BH_data_binary.pkl', 'wb') as handle:
+            pkl.dump(data, handle, protocol=pkl.HIGHEST_PROTOCOL)
+        initial_values = 'BH_data_binary.pkl'
+    else: 
+        n = n_bh # number of black holes, usually >=2
+        mass = 1.0e7  # mass of each black hole in solar mass units - same for all bodies
+        scale = 1  # scale parameter "a" for Plummer model
+        blackholes, _ = generate_plummer_initial_conditions(n, mass, scale)
+        pkl.dump({'data' : blackholes}, open(f"{path_to_save_pkl_file}/{n}body_plummer.pkl", "wb"))
+        initial_values = f'{n}body_plummer.pkl'
+    check_directory(output_folder_movie) #check if the movie dump folder exists
+
+    if do_tree:
+        check_directory(output_folder_data_tree) # check if the data tree folder exists
+        simulation(initial_file = initial_values, output_folder = output_folder_data_tree, tot_time = tot, nsteps = nstep, adaptive_dt = True, eta = eta, use_tree= True)
+        # simulation(initial_file = 'interesting_ICs_3body.pkl', output_folder = output_folder_data_tree, tot_time = tot, nsteps = nstep, adaptive_dt = True, eta = eta, use_tree= True)
+        movie_3D(tot_nstep_eta = f'{n}BH_{tot}_{nstep}_{eta}_plummer_tree', brute_or_tree="/data_tree")
+        end = time.time()
+        print(f"Execution time for BHT: {(end - start) / 60:.2f} minutes")
+
+    if do_brute:
+        check_directory(output_folder_data_brute) # check if the data brute folder exists
+        simulation(initial_file = initial_values, output_folder = output_folder_data_brute, tot_time = tot, nsteps = nstep, adaptive_dt = True, eta = eta, use_tree= False)
+        # simulation(initial_file = 'interesting_ICs_3body.pkl', output_folder = output_folder_data_brute, tot_time = tot, nsteps = nstep, adaptive_dt = True, eta = eta, use_tree= False)
+        movie_3D(tot_nstep_eta = f'{n}BH_{tot}_{nstep}_{eta}_plummer_brute', brute_or_tree="/data_brute")
+        end = time.time()
+        print(f"Execution time for brute force: {(end - start) / 60:.2f} minutes")
+
+    if do_comparison:
+        movie_3D_comparison(tot_nstep_eta = f'{n}BH_{tot}_{nstep}_{eta}_plummer_comparison')
+        end = time.time()
+        print(f"Execution time for comparison: {(end - start) / 60:.2f} minutes")
+
+# saves trajectories_{tot}_{nstep}_{eta}.mkv in the current directory
+
+test_simulation_run(n_bh=2, do_tree=True, do_brute=True, do_comparison=True, do_binary=False)
+
+#--------------------------------------------------------------------------------#
+#not needed anymore
 # np.random.seed(1)
 # init_data = generate_initial_conditions(3, 3, (1e7, 1e8), 5)
 # with open('BH_data_ic.pkl', 'wb') as f:
@@ -26,35 +108,3 @@ ureg = UnitRegistry()
 # the above must be changed to be compatible with the latest plummer IC stuff pushed to ICs.py  
 # or its output ICs for 3 BHs from before can be used, which has been manually in 'interesting_ICs_3body.pkl' 
 
-tot = 5e16
-nstep = 10
-eta = 0.1
-
-output_folder_data = "./data/"
-output_folder_movie = "./movie_dump/"
-
-def check_directory(folder_path):
-    if os.path.exists(folder_path):
-        shutil.rmtree(folder_path)  # Remove the folder and all its contents
-
-    os.makedirs(folder_path)  #make new empty folder
-
-n = 3
-mass = 1.0e7
-scale = 1
-path_to_save_pkl_file = "./"
-
-blackholes, masses = generate_plummer_initial_conditions(n, mass, scale)
-pkl.dump({'data' : blackholes}, open(f"{path_to_save_pkl_file}/{n}body_plummer.pkl", "wb"))
-
-initial_nbody = f'{n}body_plummer.pkl'
-
-check_directory(output_folder_data)
-simulation(initial_file = initial_nbody, output_folder = output_folder_data, tot_time = tot, nsteps = nstep, adaptive_dt = True, eta = eta)
-
-check_directory(output_folder_movie)
-movie_3D(tot_nstep_eta = f'{tot}_{nstep}_{eta}_plummer')
-# saves trajectories_{tot}_{nstep}_{eta}.mkv in the current directory
-# with open('BH_data_ic.pkl', 'rb') as f:
-#     ics = pickle.load(f)
-# print(ics)
---
+++ visualizations_comparisons.py
@@ -0,0 +1,158 @@
+import os
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+nbuddies_path = os.path.dirname(os.path.realpath(__file__))
+
+def movie_3D_comparison(tail_length: int = 10, tot_nstep_eta=None):
+    """
+    Loads both data_tree and data_brute simulation data and makes a combined movie
+    showing both trajectories together in 3D space with tails behind them.
+    
+    Blue - BHT (Barnes-Hut Tree)
+    Red  - BF (Brute Force)
+
+    Parameters
+    ----------
+    tail_length : int, default 10
+        Length of tail trailing behind points.
+    tot_nstep_eta: str
+        Used to dynamically save the resulting movie with info about 
+        total time (sec), num of timesteps per batch, eta for adaptive timestep computation.
+    """
+
+    # set up
+    if not os.path.exists(nbuddies_path + "/movie_dump"):  # check if dir exists
+        os.makedirs(nbuddies_path + "/movie_dump")  # if not, create dir path
+
+    # get info from sim end
+    last_batch_tree = _find_last_batch_num("/data_tree")
+    last_batch_brute = _find_last_batch_num("/data_brute")
+    last_batch_num = min(last_batch_tree, last_batch_brute)  # ensure equal frame count
+
+    # Load last batch and initialize data structures
+    with open(nbuddies_path + "/data_tree" + f"/data_batch{last_batch_num}.pkl", 'rb') as file:
+        data_tree = pickle.load(file)['data'][0]
+    with open(nbuddies_path + "/data_brute" + f"/data_batch{last_batch_num}.pkl", 'rb') as file:
+        data_brute = pickle.load(file)['data'][0]
+    N_tree = len(data_tree)
+    N_brute = len(data_brute)
+
+    # Calculate the maximum plot range based on particle positions
+    max_range = 0
+    for dataset in [data_tree, data_brute]:
+        for obj in dataset:
+            if np.linalg.norm(obj.position) > max_range:
+                max_range = np.linalg.norm(obj.position)
+    max_range *= 2  # add buffer
+
+    # get info from sim start
+    with open(nbuddies_path + "/data_tree/data_batch0.pkl", 'rb') as file:
+        init_tree = pickle.load(file)['data'][0]
+    with open(nbuddies_path + "/data_brute/data_batch0.pkl", 'rb') as file:
+        init_brute = pickle.load(file)['data'][0]
+
+    # Create 3D arrays to store tail positions
+    plotting_tree = np.zeros([N_tree, 3, tail_length])
+    plotting_brute = np.zeros([N_brute, 3, tail_length])
+    for n in range(N_tree):
+        for t in range(tail_length):
+            plotting_tree[n, :, t] = init_tree[n].position
+    for n in range(N_brute):
+        for t in range(tail_length):
+            plotting_brute[n, :, t] = init_brute[n].position
+
+    # generating movie frames
+    for i in range(last_batch_num + 1):
+
+        # slide data window forward
+        for j in range(tail_length - 1):
+            plotting_tree[:, :, j] = plotting_tree[:, :, j + 1]
+            plotting_brute[:, :, j] = plotting_brute[:, :, j + 1]
+
+        # Load current frame data
+        with open(nbuddies_path + f"/data_tree/data_batch{i}.pkl", 'rb') as file:
+            data_tree = pickle.load(file)['data'][0]
+        with open(nbuddies_path + f"/data_brute/data_batch{i}.pkl", 'rb') as file:
+            data_brute = pickle.load(file)['data'][0]
+
+        # Update tails
+        for n in range(N_tree):
+            plotting_tree[n, :, -1] = data_tree[n].position
+        for n in range(N_brute):
+            plotting_brute[n, :, -1] = data_brute[n].position
+
+        # plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # tree (blue)
+        for n in range(N_tree):
+            for t in range(tail_length - 1):
+                alpha = t / (tail_length - 2)
+                ax.plot(plotting_tree[n, 0, t:t + 2],
+                        plotting_tree[n, 1, t:t + 2],
+                        plotting_tree[n, 2, t:t + 2],
+                        'b-', alpha=alpha)
+            ax.scatter(plotting_tree[n, 0, -1],
+                       plotting_tree[n, 1, -1],
+                       plotting_tree[n, 2, -1],
+                       c="blue", s=100, marker="o")
+
+        # brute (red)
+        for n in range(N_brute):
+            for t in range(tail_length - 1):
+                alpha = t / (tail_length - 2)
+                ax.plot(plotting_brute[n, 0, t:t + 2],
+                        plotting_brute[n, 1, t:t + 2],
+                        plotting_brute[n, 2, t:t + 2],
+                        'r-', alpha=alpha)
+            ax.scatter(plotting_brute[n, 0, -1],
+                       plotting_brute[n, 1, -1],
+                       plotting_brute[n, 2, -1],
+                       c="red", s=100, marker="o")
+
+        # Set plot labels and limits
+        ax.set_xlabel('X [kpc]')
+        ax.set_ylabel('Y [kpc]')
+        ax.set_zlabel('Z [kpc]')
+        ax.set_title('Black Hole Trajectories: BHT (Blue) vs BF (Red)')
+
+        ax.set_xlim(-max_range / 2, max_range / 2)
+        ax.set_ylim(-max_range / 2, max_range / 2)
+        ax.set_zlim(-max_range / 2, max_range / 2)
+
+        # Add legend
+        blue_proxy = plt.Line2D([0], [0], linestyle='-', color='blue', label='BHT')
+        red_proxy = plt.Line2D([0], [0], linestyle='-', color='red', label='BF')
+        ax.legend(handles=[blue_proxy, red_proxy], loc='upper right')
+
+        plt.tight_layout()
+        plt.savefig(nbuddies_path + f"/movie_dump/trajectories_{i}.png",
+                    dpi=300, bbox_inches='tight')
+        plt.close()
+
+    _recompile_movie_3D_compare(tot_nstep_eta)
+
+
+def _find_last_batch_num(brute_or_tree) -> int:
+    """
+    Finds number of last batch file saved for either data_tree or data_brute
+    """
+    i = 0
+    while os.path.exists(nbuddies_path + brute_or_tree + f"/data_batch{i}.pkl"):
+        i += 1
+    return i - 1
+
+
+def _recompile_movie_3D_compare(tot_nstep_eta):
+    """
+    Deletes old comparison movie if it exists, then recreates it by compiling the PNGs in movie_dump
+    """
+    movie_path = nbuddies_path + f"/trajectories_compare_{tot_nstep_eta}.mkv"
+    if os.path.exists(movie_path):
+        os.remove(movie_path)
+    os.system("ffmpeg -framerate 12 -start_number 0 -i " +
+              nbuddies_path + "/movie_dump/trajectories_%01d.png -q:v 0 " +
+              movie_path)
