bce906a0cd31b8e44317e78b38f75be76b322b02
GoureeSankar <gouree.sath@gmail.com>
2025-11-10 13:26:49 -0800

    changed main and visualize_comp

--- main2.py
+++ main2.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(prog="NBuddies", description="Run N-body simula
 # Required
 parser.add_argument("N", type=int, help="Number of black holes in simulation")
 
-parser.add_argument("R", type=float, help="Scale Plummer sphere radius (kpc) of black holes")
+parser.add_argument("R", type=float, help="Radius (kpc) of black holes")
 
 parser.add_argument("M", type=float, help="Mass (solar mass) of black holes")
 
@@ -40,17 +40,13 @@ parser.add_argument("--eta", type=float, help="Set eta when using adaptive time 
 parser.add_argument("--use_tree", action=argparse.BooleanOptionalAction, 
                     help="Use Barnes Hut algorithm to calculate forces (default: True)", default=True)
 
-parser.add_argument("--use_leapfrog", action=argparse.BooleanOptionalAction, 
-                    help="Use Leap frog integration (default: True). False uses Euler integration", default=True)
-
 parser.add_argument("--do_comp", action=argparse.BooleanOptionalAction,
                     help="Generate comparison movie between tree and brute force methods (default: True)", default=True)
 
-parser.add_argument("--time", type = float, help="Total simulation time in seconds (default : 5e17)", default=5e17)
+parser.add_argument("--time", type = float, help="Total simulation time in seconds (default : 5e16)", default=5e17)
 
 parser.add_argument("--do_binary", action=argparse.BooleanOptionalAction,
                     help="Enable binary initial condition (default: True)", default=True)
-
 # Parse the arguments
 args = parser.parse_args()
 
@@ -64,6 +60,7 @@ data_path = nbuddies_path+"/data/"+args.name
 if not os.path.exists(data_path):
     os.makedirs(data_path)
 
+
 sim_time = args.time * ureg('second')
 
 print(f"sim time = {sim_time.to('Myr'):.3} = {sim_time.to('second'):.3}")
@@ -76,37 +73,31 @@ if args.do_binary:
                 'velocity': np.array([[0. ,3.2791 ,0.], [0. ,-3.2791 ,0.]])}
     BHs, _ = generate_binary_ICs( N_BH = 2, custom_vals = custom_vals )
     print(f"Running {args.name} with: N={custom_vals['N']}, M={custom_vals['mass']}, "
-    f"n_steps={args.n_steps}, adaptive_ts={args.adaptive_ts}, eta={args.eta}"
-    f"use_leapfrog={args.use_leapfrog}")
+    f"n_steps={args.n_steps}, adaptive_ts={args.adaptive_ts}, eta={args.eta}, use_tree={args.use_tree}")
 
 else:   
     BHs, _ = generate_plummer_initial_conditions(n_blackholes=args.N, initial_mass=(M/args.N).magnitude, 
                                                   scale=R.magnitude, ratio=args.M_ratio)
     print(f"Running {args.name} with: N={args.N}, R={args.R}, M={args.M}, "
-      f"M_ratio={args.M_ratio}, n_steps={args.n_steps}, adaptive_ts={args.adaptive_ts}, eta={args.eta}"
-      f"use_tree={args.use_tree}, use_leapfrog={args.use_leapfrog}")
+      f"M_ratio={args.M_ratio}, n_steps={args.n_steps}, adaptive_ts={args.adaptive_ts}, eta={args.eta}, use_tree={args.use_tree}")
 
 
 pkl.dump(BHs, open(data_path+"/ICs.pkl", "wb"))
 
-
-# #calc relax time
+#calc relax time
 # t_relax = 0.14*args.N * (R**(3/2)) / (np.log(0.4*args.N) * np.sqrt(GG*M))
 # print(t_relax.to("Myr"))
 
 # t_segregate = 0.1 * t_relax
 # sim_time = 3*t_relax
 
-# print(f"sim time = {sim_time.to('Myr'):.3} = {sim_time.to('second'):.3}")
-
 #run_sim
 if args.do_comp:
+    
     simulation(data_path+"/ICs.pkl", data_path+"_tree", tot_time=sim_time.to('second').magnitude, nsteps=args.n_steps, 
         adaptive_dt=args.adaptive_ts, eta=args.eta, use_tree=True)
-    movie_3D(args.name+"_tree")
     simulation(data_path+"/ICs.pkl", data_path+"_brute", tot_time=sim_time.to('second').magnitude, nsteps=args.n_steps, 
         adaptive_dt=args.adaptive_ts, eta=args.eta, use_tree=False)
-    movie_3D(args.name+"_brute")
     movie_3D_comparison(args.name)
 else:
     simulation(data_path+"/ICs.pkl", data_path, tot_time=sim_time.to('second').magnitude, nsteps=args.n_steps, 
@@ -116,5 +107,7 @@ else:
 
 
 ################################ FIX BELOW ################################
+use_leap_frog = True # Use Leapfrog integration
+use_Euler = False # Use Euler integration
 alpha = ALPHA # Set tree parameter in dynamic criterion
-theta = THETA_0 # Set threshold parameter in Barnes Hut algorithm 
+theta = THETA_0 # Set threshold parameter in Barnes Hut algorithm
